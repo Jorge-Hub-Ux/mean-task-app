0decdc03c95787943a559ebad36cd79762bc5e43
Jorge Ramirez <jorge.ramirez@btilab.co>
2025-06-03 00:23:31 -0500

    feat: setup folder structure and, add offline components and error interceptor

--- frontend-tasks-mean/src/app/app.component.ts
+++ frontend-tasks-mean/src/app/app.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
+import { fromEvent, map, merge, of } from 'rxjs';
+import { OfflineComponent } from './shared/ui/components/offline/offline.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [RouterOutlet, OfflineComponent],
+  styleUrl: './app.component.scss',
+  template: `
+    @if (isOnline()) {
+      <router-outlet />
+    } @else {
+      <app-offline />
+    }
+  `,
 })
 export class AppComponent {
-  title = 'frontend-tasks-mean';
+  isOnline = signal<boolean>(true);
+
+  constructor() {
+    this.checkInternetStatus();
+  }
+
+  private checkInternetStatus = () => {
+    merge(
+      of(navigator.onLine),
+      fromEvent(window, 'online').pipe(map(e => !!e)),
+      fromEvent(window, 'offline').pipe(map(e => !e)),
+    )
+      .pipe(takeUntilDestroyed())
+      .subscribe({
+        next: isOnline => {
+          this.isOnline.set(isOnline);
+        },
+      });
+  };
+
 }
---
+++ frontend-tasks-mean/src/app/core/interceptors/error/error.interceptor.ts
@@ -0,0 +1,45 @@
+import { HttpErrorResponse, HttpInterceptorFn, HttpStatusCode } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+export type ErrorType = {
+  errorTitle: string;
+  message: string;
+  errors?: Record<string, string>;
+  controls?: Record<string, string> | null;
+  statusCode?: number;
+};
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      console.error(`[HTTP ERROR] ${error.status} - ${error.message}`);
+
+      const formattedError: ErrorType = {
+        errorTitle: getErrorTitle(error.status),
+        message: getErrorMessage(error),
+        statusCode: error.status,
+      };
+
+      return throwError(() => formattedError);
+    })
+  );
+};
+
+const getErrorTitle = (status: number): string => {
+  switch (status) {
+    case HttpStatusCode.BadRequest:
+      return 'Bad Request';
+    case HttpStatusCode.Unauthorized:
+      return 'Unauthorized';
+    case HttpStatusCode.NotFound:
+      return 'Not Found';
+    case HttpStatusCode.InternalServerError:
+      return 'Server Error';
+    default:
+      return 'Unexpected Error';
+  }
+};
+
+const getErrorMessage = (error: HttpErrorResponse): string => {
+  return error.error?.message || 'Something went wrong. Please try again.';
+};
